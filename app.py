30ea04ce82b39a3b0359baafb5a074c58dcb4495
MuzammilShah47 <muzammilshah47rgc@gmail.com>
2024-05-27 20:32:56 +0500

    info added

--- app.py
+++ app.py
@@ -9,15 +9,21 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     hotjar_site_id = os.getenv('HOTJAR_SITE_ID')
-    return render_template('index.html', hotjar_site_id=hotjar_site_id)
+    user_id = get_user_id()  # Replace with your actual logic to get the user ID
+    return render_template('index.html', hotjar_site_id=hotjar_site_id, user_id=user_id)
 
 @app.route('/predict', methods=["POST"])
 def predict():
     hotjar_site_id = os.getenv('HOTJAR_SITE_ID')
+    user_id = get_user_id()  # Replace with your actual logic to get the user ID
     feature = [int(x) for x in request.form.values()]
     feature_final = np.array(feature).reshape(-1, 1)
     prediction = model.predict(feature_final)
-    return render_template('index.html', hotjar_site_id=hotjar_site_id, prediction_text='Price of House will be Rs. {}'.format(int(prediction)))
+    return render_template('index.html', hotjar_site_id=hotjar_site_id, user_id=user_id, prediction_text='Price of House will be Rs. {}'.format(int(prediction)))
+
+def get_user_id():
+    # Dummy function for example purposes, replace with actual user ID retrieval logic
+    return 12345  # Example user ID
 
 if __name__ == '__main__':
     app.run(debug=True)
